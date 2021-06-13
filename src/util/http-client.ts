7000f750d20778b435db8886444010a67214fbf9
Mohamed Sayed <mohamed.sayed@tradeling.com>
2021-06-13 15:11:36 +0400

    feat: http-client util

--- src/util/http-client.ts
+++ src/util/http-client.ts
@@ -1,6 +1,5 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
-// https://gh-trending-api.herokuapp.com
 const axiosClient = axios.create({
   baseURL: 'http://localhost:3000/api', //only for local ( we should add baseURL based on the ENV variable.
   timeout: 10000,
